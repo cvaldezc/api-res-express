4ce1c271b473639af94a1cf77a321fb73722a56a
Christian Valdez <cvaldezchavez@gmail.com>
2017-08-05 13:49:33 -0500

    worked with json web token and services

---
+++ controllers/user.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const User = require('../models/user')
+const services = require('../services')
+
+function signUp(req, res) {
+    const user = new User({
+        email: req.body.email,
+        displayname: req.body.displayname
+    })
+    user.save(err => {
+        if (err) return res.status(500).send({message: `Error at create user ${err}`})
+
+        res.status(200).send({ token: services.createToken(user) })
+    })
+}
+
+function signIn(req, res) {
+    User.findById({email: req.body.email}, (err, user) => {
+        if (err) return res.status(500).send({message: `Error ${err}`})
+        if (!user) return res.status(404).send({message: 'user not found'})
+
+        req.user = user
+        res.status(200).send({
+            message: 'login successful',
+            token: services.createToken(user)
+        })
+    })
+}
+
+module.exports = {
+    signUp,
+    signIn
+}
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@
 
 const express = require('express')
 const ProductCtrl = require('../controllers/product')
+const auth = require('../middlewares/auth')
 const api = express.Router()
 
 api.get('/products', ProductCtrl.getProducts);
@@ -9,6 +10,10 @@ api.get('/product/:productid', ProductCtrl.getProduct);
 api.post('/product', ProductCtrl.createProduct);
 api.put('/product/:productid', ProductCtrl.updateProduct);
 api.delete('/product/:productid', ProductCtrl.deleteProduct);
+
+api.get('/private', auth, (req, res) => {
+    res.status(200).send({message: 'You allow acess'})
+})
 
 
 module.exports = api
